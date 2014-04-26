a54d3deb1329cc5f19d04bcf52b12824aea33af0
Zoe <dadac123@gmail.com>
2014-04-26 14:24:45 +0800

    index

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,50 +1,34 @@
 import media
 import fresh_tomatoes
-
-toy_story = media.Movie("Toy Story",
-                        "A Story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "http://www.youtube.com/watch?v=vwyZH85NQC4")
-#print(toy_story.storyline)
-
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                     "http://www.youtube.com/watch?v=-9ceBgWV8io")
-#print(avatar.storyline)
-
-#avatar.show_trailer()
-
-
-
-
-
-
+ 
 my_own_private_idaho = media.Movie("My Own Private Idaho",
                                    "River Phoenix and Keanu Reeves",
                                    "http://movieposters.2038.net/p/My-Own-Private-Idaho.jpg",
                                    "http://www.youtube.com/watch?v=QQbq2kl-P-4")
-#my_own_private_idaho.show_trailer()
-
-
-school_of_rock = media.Movie("School of Rock",
-                             "Using rock music to learn",
-                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
-                             "http://www.youtube.com/watch?v=3PsUJFEBC74")
-
-
-ratatouille = media.Movie("Ratatouille", "Storyline",
-                          "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
-                          "http://www.youtube.com/watch?v=c3sBBRxDAqk")
-
-midnight_in_paris = media.Movie("Midnight in Paris", "Storyline",
-                                "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
-                                "http://www.youtube.com/watch?v=atLg2wQQxvu")
-
-hunger_games = media.Movie("Hunger Games","Storyline",
-                           "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
-                           "http://www.youtube.com/watch?v=PbA63a7H0bo")
-
-movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris,
-          hunger_games, my_own_private_idaho]
+ 
+the_edukators = media.Movie("The Edukators",
+                            "The Edukators (Die Fetten Jahre sind vorbei)",
+                            "http://germanics.washington.edu/sites/germanics/files/images/die_fetten_jahre_sind_vorbei.jpg",
+                            "https://www.youtube.com/watch?v=MB1UMfC8koc")
+ 
+garden_state = media.Movie("Garden State",
+                           "Garden State",
+                           "http://d3gtl9l2a4fn1j.cloudfront.net/t/p/original/jqfhv16LODEhj04E4EZ1swFRZWX.jpg",
+                           "https://www.youtube.com/watch?v=u82n0e1mgmQ")
+ 
+little_miss_sunshine = media.Movie("Little Miss Sunshine",
+                                   "Little Miss Sunshine",
+                                   "http://nomorechildhood.files.wordpress.com/2011/03/little_miss_sunshine_ver5.jpg",
+                                   "https://www.youtube.com/watch?v=GOQrrlz-u_I")
+good_will_hunting = media.Movie("Good Will Hunting","",
+                                "http://ia.media-imdb.com/images/M/MV5BMTk0NjY0Mzg5MF5BMl5BanBnXkFtZTcwNzM1OTM2MQ@@._V1_SY317_CR1,0,214,317_AL_.jpg",
+                                "https://www.youtube.com/watch?v=WDcMUCpppVs")
+ 
+life_is_beautiful = media.Movie("Life is Beautiful",
+                                "",
+                                "http://upload.wikimedia.org/wikipedia/en/thumb/7/7c/Vitaebella.jpg/220px-Vitaebella.jpg",
+                                "https://www.youtube.com/watch?v=16RZHqCIy9M")
+ 
+movies = [my_own_private_idaho, life_is_beautiful, little_miss_sunshine, the_edukators, garden_state,
+           good_will_hunting]
 fresh_tomatoes.open_movies_page(movies)
